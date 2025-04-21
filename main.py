a89e9fa16d6a376dde5353ec567dac11423f7c7f
Noam <noam.chamouard@etudiant.univ-lr.fr>
2025-04-21 10:58:38 +0200

    meilleur affichage

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from utils import find_closest_trees, extract_data_for_city
 
 def main():
     city = str(input("Dans quelle ville souhaitez vous ajouter des arbres ? : "))
+    nb_trees = int(input("Combien d'arbres différents voulez vous voir ? : "))
     city_data = extract_data_for_city(city) #climat de la ville
-    best_trees = find_closest_trees(city_data,20) #meilleurs arbres pour ce climat
+    best_trees = find_closest_trees(city_data,nb_trees) #meilleurs arbres pour ce climat
 
-    print("arbres les mieux adaptés aux conditions climatiques de votre ville : ")
+    print(f"\n{nb_trees} Arbres les mieux adaptés aux conditions climatiques de {city} : ")
     for tree, dist in best_trees:
-        print(f"arbre : {tree}, distance avec caractéristiques de la ville : {dist}")
+        print(f"{tree}, distance avec caractéristiques de la ville : {dist}")
     return
 
 main()
