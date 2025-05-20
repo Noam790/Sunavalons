bb13ca48d3edb29730708ca8bdcb0287571d5950
Dunyach Raphaël <dunyachr@gmail.com>
2025-05-20 08:51:31 +0200

    amélioration de l'html du header et du footer

--- web/components/header.php
+++ web/components/header.php
@@ -1,13 +1,12 @@
 <header>
     <div class="header-container">
         <img src="assets/logo.png">
-        <h1 class="header-title">Sunavalons</h1>
+        <h1 class="titre"><a href="index.php">Sunavalons</a></h1>
         <nav>
             <ul class="nav-links">
-                <li><a href="index.php" class="nav-link">Accueil</a></li>
-                <li><a href="plantator.php" class="nav-link">Plantator</a></li>
-                <li><a href="comparator.php" class="nav-link">Comparator</a></li>
-                <li><a href="a_propos.php" class="nav-link">A propos</a></li>
+                <li><a href="plantator.php">Plantator</a></li>
+                <li><a href="">Outil 2</a></li>
+                <li><a href="">Outil 3</a></li>
             </ul>
         </nav>
     </div>
