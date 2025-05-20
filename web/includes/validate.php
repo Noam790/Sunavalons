355858afcdc9946232ad206046297bd8a8a0f5ef
Dunyach Raphaël <dunyachr@gmail.com>
2025-05-20 09:56:26 +0200

    petite correction du style pour téléphone du header + d'autres fix

--- web/includes/validate.php
+++ web/includes/validate.php
@@ -1,5 +1,5 @@
 <?php
 function validate_ville($ville) {
-    $ville = urlencode($ville);
+    $ville = urldecode($ville);
     return is_string($ville) && preg_match('/^[a-zA-Z_\- ]+$/u', $ville);
 }
