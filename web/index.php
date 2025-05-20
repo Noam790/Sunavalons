825770a1309a49082021c0ba5dd00d74f87938a4
Dunyach Raphaël <dunyachr@gmail.com>
2025-05-20 10:30:41 +0200

    ajout d'un fichier pour le style de l'index

--- web/index.php
+++ web/index.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="style/styleHeaderFooter.css">
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/styleIndex.css">
 
     <title>Sunavalons: Page principale</title>
 </head>
