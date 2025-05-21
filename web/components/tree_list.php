0b61207bf8570ddcca85d06b42fd2ca997077079
Noam <noam.chamouard@etudiant.univ-lr.fr>
2025-05-21 21:20:23 +0200

    fix css & sizes

--- web/components/tree_list.php
+++ web/components/tree_list.php
@@ -1,6 +1,5 @@
 <h2><?= $title ?></h2>
 <div class="card-table-plantator">
-    
     <?php foreach ($trees as $tree_data): ?>
         <?= render_tree_card($tree_data, $image_path); ?>
     <?php endforeach; ?>
